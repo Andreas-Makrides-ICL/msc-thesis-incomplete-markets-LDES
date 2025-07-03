a331b8c764990f45ab119566827231ee66269548
AndreasMakrides <amakri09@ucy.ac.cy>
2025-07-03 18:39:52 +0300

    -
    -

--- models/Fully_incomplete_markets_ADMM/data_final/convert_to_longformat.py
+++ models/Fully_incomplete_markets_ADMM/data_final/convert_to_longformat.py
@@ -8,7 +8,7 @@ Created on Thu Jun  5 10:20:19 2025
 import pandas as pd
 
 # Load your data
-df = pd.read_csv(r"C:\Users\user\Desktop\msc-thesis-incomplete-markets-LDES\models\Fully_incomplete_markets_ADMM\data_final\concatenated_capacity_factors_672_30yr_new.csv") # Replace with your actual filename
+df = pd.read_csv(r"C:\Users\user\Desktop\msc-thesis-incomplete-markets-LDES\models\Fully_incomplete_markets_ADMM\data_final\concatenated_capacity_factors_672_30yr_new_final.csv") # Replace with your actual filename
 
 # Convert from wide to long format
 long_df = df.melt(id_vars=["Y", "T"], var_name="G", value_name="value")
@@ -17,4 +17,4 @@ long_df = df.melt(id_vars=["Y", "T"], var_name="G", value_name="value")
 long_df = long_df.rename(columns={"Y": "O"})
 
 # Save to new CSV
-long_df.to_csv(r"C:\Users\user\Desktop\msc-thesis-incomplete-markets-LDES\models\Fully_incomplete_markets_ADMM\data_final\concatenated_capacity_factors_672_30yr_new_lf.csv", index=False)
+long_df.to_csv(r"C:\Users\user\Desktop\msc-thesis-incomplete-markets-LDES\models\Fully_incomplete_markets_ADMM\data_final\concatenated_capacity_factors_672_30yr_new_final_lf.csv", index=False)
