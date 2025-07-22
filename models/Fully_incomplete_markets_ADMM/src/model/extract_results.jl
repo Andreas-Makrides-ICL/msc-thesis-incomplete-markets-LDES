5af1364d4202078c96bc229c10ef8856d2c19e6e
AndreasMakrides <amakri09@ucy.ac.cy>
2025-07-22 17:58:32 +0300

    -
    -

--- models/Fully_incomplete_markets_ADMM/src/model/extract_results.jl
+++ models/Fully_incomplete_markets_ADMM/src/model/extract_results.jl
@@ -57,16 +57,19 @@ function extract_price!(model; verbose = false)
     m = model.model
     data = model.data
 
+    δ = data["data"]["additional_params"]["δ"]
     # Check if the demand_balance constraint exists in the model
     if haskey(m, :demand_balance)
         # Extract shadow prices (dual values) from the demand balance constraint
         price = dual.(m[:demand_balance])
+        dual_vals = Dict(o => dual(m[:cvar_tail_total][o]) for o in O)
+
         
         O = data["sets"]["O"]
         T = data["sets"]["T"]
         # Adjust prices by weights and probabilities if specified
         for t in T, o in O
-            price[t, o] /= data["data"]["time_weights"][t,o] * data["data"]["additional_params"]["P"][o]  #This step is needed because your model likely multiplies constraints and objectives by weights during optimization.
+            price[t, o] /= data["data"]["time_weights"][t,o] * (data["data"]["additional_params"]["P"][o] * δ + dual_vals[o]) #This step is needed because your model likely multiplies constraints and objectives by weights during optimization.
         end
 
         # Ensure prices are positive by negating if the average is negative
@@ -224,8 +227,8 @@ function extract_unserved_demand(model)
     unserved_demand_fix = Dict(o => sum(W[t,o] *  value(m[:unserved_fixed][t,o]) for t in T) for o in O)
     unserved_demand_flex = Dict(o => sum(W[t,o] *  value(m[:unserved_flex][t,o]) for t in T) for o in O)
 
-    total_unserved_demand_fix = sum(unserved_demand_fix[o] for o in O)
-    total_unserved_demand_flex = sum(unserved_demand_flex[o] for o in O) 
+    total_unserved_demand_fix = sum(P[o] * unserved_demand_fix[o] for o in O)
+    total_unserved_demand_flex = sum(P[o] * unserved_demand_flex[o] for o in O) 
     total = total_unserved_demand_fix + total_unserved_demand_flex
     
     # Print summary
