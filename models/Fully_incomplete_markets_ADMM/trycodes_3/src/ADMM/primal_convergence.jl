242d688003dc6dff7aba4c248286c979ec797dd2
AndreasMakrides <amakri09@ucy.ac.cy>
2025-08-22 17:30:23 +0300

    -
    -

--- models/Fully_incomplete_markets_ADMM/trycodes_3/src/ADMM/primal_convergence.jl
+++
@@ -1,9 +0,0 @@
-function calculate_primal_convergence(m, iteration)
-
-    residual = m.results[iteration][:residual]
-    primal_convergence = norm(residual, 2)
-
-    # Store primal convergence in the results
-    m.results[iteration][:primal_convergence] = primal_convergence
-    return primal_convergence
-end
